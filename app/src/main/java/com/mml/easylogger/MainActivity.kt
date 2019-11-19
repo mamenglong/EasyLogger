ed31c512cde2f06a365e2af8e09796d636a360d7
Meng long <mml2015@126.com>
2019-11-19 12:02:14 +0800

    Initial commit

---
+++ app/src/main/java/com/mml/easylogger/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.mml.easylogger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.FileUtils
+import com.mml.loggerlibrary.Logger
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Logger.debug(BuildConfig.DEBUG)
+            .saveToDisk(true,this)
+        Logger.i("onCreate")
+        Logger.i("onCreate","MainActivity")
+
+    }
+}
